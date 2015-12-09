b237602b121773ce3a92741370a34e94c5e75218
Florian Oellerich <f.oellerich@epages.com>
2015-12-09 16:23:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# scribe-plugin-tab-indent
+A plugin for [scribe](https://github.com/guardian/scribe/) that makes indention with tab-key possible.
+
+## Usage
+```javascript
+var scribeTabIndent = require('scribe-plugin-tab-indent');
+scribe.use(scribeTabIndent());
+```
+
+## Installation
+```
+bower install scribe-plugin-tab-indent
+```
---
+++ src/scribe-plugin-tab-indent.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    var scribePluginTabIndent = function () {
+        return function (scribe) {
+            scribe.el.addEventListener('keydown', function (event) {
+                // tab pressed
+                if (event.keyCode === 9) {
+                    event.preventDefault();
+
+                    // check if current selection is inside a list
+                    const listActive =
+                        scribe.getCommand('insertUnorderedList').queryState() ||
+                        scribe.getCommand('insertOrderedList').queryState();
+
+                    if (event.shiftKey) {
+                        // when shift is also pressed do an outdent
+                        scribe.getCommand('outdent').execute();
+                    } else if (listActive) {
+                        // when we are in a list indent it by one
+                        scribe.getCommand('indent').execute();
+                    }
+                }
+            });
+        };
+    };
+
+    // Module system magic dance
+    if (typeof module !== 'undefined') {
+        module.exports = scribePluginTabIndent;
+    } else if (typeof define === 'function' && typeof define.amd === 'object') {
+        define(function () {
+            return scribePluginTabIndent;
+        });
+    } else {
+        window.scribePluginTabIndent = scribePluginTabIndent;
+    }
+}());
